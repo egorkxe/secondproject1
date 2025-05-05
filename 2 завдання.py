0eda706260d49a89cb0e93f9902637e5bc675749
egorkxe <1911tev@gmail.com>
2025-05-05 23:30:54 +0300

    first commit

--- 2 завдання.py
+++ 2 завдання.py
@@ -1,1 +1,10 @@
-print("Hello world")
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def get_info(self):
+        return f"{self.make} {self.model} {self.year}"
+
+my_car = Car("Toyota", "Camry", 2020)
+print(my_car.get_info())
